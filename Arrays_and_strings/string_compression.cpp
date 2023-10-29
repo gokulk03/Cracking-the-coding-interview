00a43d691057523a35c0c83dfc1d410580b77414
Gokul <gokulsrinivask2020@gmail.com>
2023-10-29 20:06:15 +0530

    string_compression_added

---
+++ Arrays_and_strings/string_compression.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i=0;
+        for(int j=1,count=1;j<=chars.size();j++,count++){
+            if(j==chars.size() || chars[j]!=chars[j-1]){
+                chars[i]=chars[j-1];
+                i++;
+                if(count>=2){
+                    for(char digit:to_string(count)){
+                        chars[i]=digit;
+                        i++;
+                    }
+                }
+                count=0;
+            }
+
+        }
+        return i;
+
+    }
+};
